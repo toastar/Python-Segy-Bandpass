2d93055bb76596677f07fdeb637509001a2d9b40
Toastar <toastar@gmail.com>
2014-01-10 12:15:16 -0600

    Update freq.py

--- freq.py
+++ freq.py
@@ -1,9 +1,29 @@
-from numpy import sin, linspace, pi, array, empty
+from numpy import sin, linspace, pi, array, empty, pi
 from pylab import plot, show, title, xlabel, ylabel, subplot
-from scipy import fft, arange
+from scipy import fft, arange, signal
 from obspy.segy.core import readSEGY
 
+
+
+
+
+#
+# Input Parameters Block
+#
+
 filename='test.sgy'; 
+
+Lowcut = 8          # Low frequency
+Highcut = 60        # High frequency
+
+order = 5           # Higher number means a harsher cutoff
+
+
+#
+#
+#
+
+
 
 
 
@@ -36,15 +56,50 @@ Fs = 250.0;  # sampling rate ( Samples per second.)
 
 datalen = len(Data) #Gets the length of the trace
 count = arange(datalen)  
-rate = count.astype(float)  / 250.0 # turns sample number into Seconds
+rate = -1* count.astype(float)  / 250.0 # turns sample number into Seconds
 
 # End number crunching and begin buiding plots.
 
-subplot(2,1,1)
-plot(rate,Data)
-xlabel('Time')
-ylabel('Amplitude')
-subplot(2,1,2)
+subplot(2,2,1)
+plot(Data,rate)
+ylabel('Time')
+xlabel('Amplitude')
+subplot(6,1,4)
 plotSpectrum(Data,Fs)
+show()
+
+
+#Begin Bandpass
+
+low = Lowcut * 2 / Fs           # 2 / Fs is the nyquest freq / First number is the pass freq
+high = Highcut  * 2 / Fs
+
+
+subplot(2,2,2)                                         
+
+b, a = signal.butter(order, [low,high], btype='band') # Build the filter
+
+BP_Data = signal.lfilter(b, a, Data)                          # Filter Data
+
+plot(BP_Data,rate)                    
+
+
+
+subplot(6,1,6)
+
+# Plot Bandpass
+
+#plot(Data[::-1],rate)
+ylabel('Time')
+xlabel('Amplitude')
+
+plotSpectrum(BP_Data,Fs)
+
+w, h = signal.freqz(b, a, worN=2000)
+
+
+subplot(6,1,5)
+plot((Fs * 0.5 / pi) * w, abs(h))
+
 show()
 
